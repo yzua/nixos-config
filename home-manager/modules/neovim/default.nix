d391ec7257da898ac4a89d043fb1325f96e478eb
yz <260740417+yzua@users.noreply.github.com>
2026-05-07 00:46:52 +0300

    refactor(hm/neovim): extract treesitter parsers into let binding

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -26,6 +26,35 @@ let
     ./lua/plugins/dap.lua
   ];
   initLuaBundle = builtins.concatStringsSep "" (map lua luaModules);
+  treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+    typescript
+    tsx
+    javascript
+    rust
+    zig
+    pkgs.vimPlugins.nvim-treesitter-parsers.lua
+    nix
+    json
+    yaml
+    toml
+    markdown
+    markdown_inline
+    bash
+    html
+    css
+    python
+    go
+    vim
+    vimdoc
+    regex
+    c
+    cpp
+    java
+    svelte
+    graphql
+    dockerfile
+  ];
+  treesitterQueries = map (parser: parser.associatedQuery) treesitterParsers;
 in
 {
   imports = [
@@ -49,68 +78,43 @@ in
     withRuby = false;
     withPython3 = false;
 
-    plugins = with pkgs.vimPlugins; [
-      plenary-nvim
-      nvim-web-devicons
-      nui-nvim
+    plugins =
+      (with pkgs.vimPlugins; [
+        plenary-nvim
+        nvim-web-devicons
+        nui-nvim
 
-      nvim-treesitter
-      nvim-treesitter-parsers.typescript
-      nvim-treesitter-parsers.tsx
-      nvim-treesitter-parsers.javascript
-      nvim-treesitter-parsers.rust
-      nvim-treesitter-parsers.zig
-      nvim-treesitter-parsers.lua
-      nvim-treesitter-parsers.nix
-      nvim-treesitter-parsers.json
-      nvim-treesitter-parsers.yaml
-      nvim-treesitter-parsers.toml
-      nvim-treesitter-parsers.markdown
-      nvim-treesitter-parsers.markdown_inline
-      nvim-treesitter-parsers.bash
-      nvim-treesitter-parsers.html
-      nvim-treesitter-parsers.css
-      nvim-treesitter-parsers.python
-      nvim-treesitter-parsers.go
-      nvim-treesitter-parsers.vim
-      nvim-treesitter-parsers.vimdoc
-      nvim-treesitter-parsers.regex
-      nvim-treesitter-parsers.c
-      nvim-treesitter-parsers.cpp
-      nvim-treesitter-parsers.java
-      nvim-treesitter-parsers.svelte
-      nvim-treesitter-parsers.graphql
-      nvim-treesitter-parsers.dockerfile
+        nvim-lspconfig
+        conform-nvim
+        nvim-lint
+        trouble-nvim
 
-      nvim-lspconfig
-      conform-nvim
-      nvim-lint
-      trouble-nvim
+        nvim-dap
+        nvim-dap-ui
+        nvim-dap-virtual-text
 
-      nvim-dap
-      nvim-dap-ui
-      nvim-dap-virtual-text
+        nvim-cmp
+        cmp-nvim-lsp
+        cmp-buffer
+        cmp-path
+        cmp_luasnip
+        luasnip
+        friendly-snippets
 
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp-buffer
-      cmp-path
-      cmp_luasnip
-      luasnip
-      friendly-snippets
+        telescope-nvim
+        neo-tree-nvim
 
-      telescope-nvim
-      neo-tree-nvim
-
-      gitsigns-nvim
-      lualine-nvim
-      which-key-nvim
-      indent-blankline-nvim
-      comment-nvim
-      nvim-autopairs
-      nvim-surround
-      vim-sleuth
-    ];
+        gitsigns-nvim
+        lualine-nvim
+        which-key-nvim
+        indent-blankline-nvim
+        comment-nvim
+        nvim-autopairs
+        nvim-surround
+        vim-sleuth
+      ])
+      ++ treesitterParsers
+      ++ treesitterQueries;
 
     initLua = initLuaBundle;
   };
