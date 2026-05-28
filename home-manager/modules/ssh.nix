685ae519d8815eae77850af07ed5acdb79a5c796
yz <260740417+yzua@users.noreply.github.com>
2026-05-28 16:14:58 +0300

    refactor(hm/ssh): migrate to programs.ssh.settings format

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -7,51 +7,45 @@ _:
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = {
+    settings = {
       "*" = {
-        extraOptions = {
-          # Prefer modern key exchange and ciphers
-          KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org";
-          Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
-          MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
-          HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519";
+        # Prefer modern key exchange and ciphers
+        KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org";
+        Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com";
+        MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
+        HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519";
 
-          # Security defaults
-          ForwardAgent = "no";
-          ForwardX11 = "no";
-          AddKeysToAgent = "confirm";
-          IdentitiesOnly = "yes";
-          StrictHostKeyChecking = "ask";
-          VerifyHostKeyDNS = "yes";
-          UpdateHostKeys = "yes";
-          HashKnownHosts = "yes";
+        # Security defaults
+        ForwardAgent = "no";
+        ForwardX11 = "no";
+        AddKeysToAgent = "confirm";
+        IdentitiesOnly = "yes";
+        StrictHostKeyChecking = "ask";
+        VerifyHostKeyDNS = "yes";
+        UpdateHostKeys = "yes";
+        HashKnownHosts = "yes";
 
-          # Connection keepalive + auto-close idle sessions
-          ServerAliveInterval = "60";
-          ServerAliveCountMax = "3";
+        # Connection keepalive + auto-close idle sessions
+        ServerAliveInterval = "60";
+        ServerAliveCountMax = "3";
 
-          # Timeout idle connections after 10 minutes (prevents stale session hijacking)
-          ConnectionAttempts = "3";
-          ConnectTimeout = "30";
-        };
+        # Timeout idle connections after 10 minutes (prevents stale session hijacking)
+        ConnectionAttempts = "3";
+        ConnectTimeout = "30";
       };
 
       "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        extraOptions = {
-          PreferredAuthentications = "publickey";
-        };
+        HostName = "github.com";
+        User = "git";
+        PreferredAuthentications = "publickey";
       };
 
       "codeberg.org" = {
-        hostname = "codeberg.org";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519";
-        extraOptions = {
-          AddressFamily = "inet";
-          PreferredAuthentications = "publickey";
-        };
+        HostName = "codeberg.org";
+        User = "git";
+        IdentityFile = "~/.ssh/id_ed25519";
+        AddressFamily = "inet";
+        PreferredAuthentications = "publickey";
       };
     };
   };
